76d28c18495c53b78c8b537e991cee972142d6a7
BearPanther <github@bearpanther.com>
2013-03-03 14:58:41 -0800

    Added timer and score as well as right/wrong colors after typing a state abbr.

--- js/whack.js
+++ js/whack.js
@@ -2,12 +2,15 @@ require([
   "dojo/_base/array", 
   "dojo/_base/fx", 
   "dojo/dom", 
+  "dojo/dom-attr", 
+  "dojo/dom-class", 
   "dojo/dom-style", 
+  "dojo/number", 
   "dojo/on", 
   "dojo/query", 
   "dojo/domReady!"
 ], function(
-  array, fx, dom, domStyle, on, query
+  array, fx, dom, domAttr, domClass, domStyle, number, on, query
 ) {
   var currentState; // used to store the states two letter abbreviation
   var state; // the element representing the state
@@ -16,12 +19,87 @@ require([
   var indexes= array.map(states, function(state, idx) { return idx; });
   var hint;
   var meta = query(".meta")[0];
-  var box = query(".wrapper input")[0];
+
+  // keep track of the score and the timer
+  var right = 0;
+  var tried = 0;
+  var score = query(".score")[0];
+  var timer = query(".timer")[0];
+  var seconds = 0;
+  var timerUpdate; // handle to setInterval used to make the timer run
+  var timing = false;
+
+  // colors for current, right and wrong
+  var currentColor = "#5DADDD";
+  var rightColor = "#66CA66";
+  var wrongColor = "#CC4A4A";
+
   // make it easy to start typing
+  var box = query(".wrapper input")[0];
   box.focus();
 
   // highligh a state
   animateState();
+
+  stateChecker = on(box, "keyup", function(e) {
+    if ( (e.keyCode || e.charCode) === 13 ) {
+      if ( !timing ) {
+        startTimer();
+        timing = true;
+      }
+      tried = tried + 1;
+      // check answer
+      var state = query("li." + currentState)[0];
+      if ( this.value.toLowerCase() === currentState ) {
+        right = right + 1;
+        // correct answer, make the state green
+        domStyle.set(state, "color", rightColor);
+        meta.innerHTML = "Yep.";
+        this.value = "";
+      } else {
+        // wrong answer
+        domStyle.set(state, "color", wrongColor);
+        meta.innerHTML = "Nope.";
+        this.value =="";
+      }
+      // reset the value
+      this.value = "";
+      // highlight a new state, if there are states left
+      if ( indexes.length === 0 ) {
+        // remove the event listener after going through all states
+        stateChecker.remove();
+        // console.log("removed evt listener");
+        // stop the timer
+        quit("Finished.");
+      } else {
+        // highlight another state
+        animateState();
+      }
+      score.innerHTML = right + " / " + tried;
+    }
+  });
+
+  // set up the timer
+  // rudimentary...but good enough!
+  function startTimer() {
+    timerUpdate = setInterval(function() {
+      seconds = seconds + 1;
+      var minutes = parseInt(seconds / 60);
+      timer.innerHTML = "" + minutes + ":" +
+        number.format(seconds % 60, { pattern: "00" });
+      
+      // 5 minute max...
+      if ( minutes === 5 ) {
+        quit("Out of time. Try again.");
+      }
+    }, 1000);
+  }
+
+  function quit(message) {
+    clearInterval(timerUpdate);
+    domAttr.set(box, "disabled", true);
+    meta.innerHTML = message;
+  }
 
   function animateState() {
     // new random number
@@ -34,40 +112,7 @@ require([
     currentState = state.dataset.state;
     // animate the state
     // fx.anim args:  node, properties, duration, easing, onEnd, delay
-    fx.anim(state, { color: "#5DADDD" }, 100); // blue
+    fx.anim(state, { color: currentColor }, 100); // blue
   }
 
-  stateChecker = on(box, "keyup", function(e) {
-    clearTimeout(hint);
-    // console.log("box key up: ", e, e.value, this.value);
-    // check the answer
-    if ( this.value.toLowerCase() === currentState ) {
-      meta.innerHTML = "YEP.";
-      // correct answer, make the state green
-      domStyle.set(state, "color", "#66CA66");
-      this.value = "";
-      // remove the event listener after going through all states
-      if ( indexes.length === 0 ) {
-        stateChecker.remove();
-        meta.innerHTML = "Finished. Well done.";
-        console.log("removed evt listener");
-      } else {
-        // highlight another state
-        animateState();
-      }
-      return;
-    }
-    if ( this.value.length === 1 ) {
-      meta.innerHTML = "";
-      return;
-    }
-    if ( this.value.length >= 2 ) {
-      meta.innerHTML = "Nope.";
-      return;
-    }
-  });
-
-  hint = setTimeout(function() {
-    meta.innerHTML = "Hey! Let's go! Type \"" + currentState.toUpperCase() + "\" in the box.<br><br>" + meta.innerHTML;
-  }, 5000)
 });
